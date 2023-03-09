d10868e5aa1c9aa8fad23e07dc019fd28ea5b6f6
KowreeshMK0202 <122335440+KowreeshMK0202@users.noreply.github.com>
2023-03-09 01:35:46 -0800

    Add files via upload

---
+++ BubbleSort2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ 
+class BubbleSort1 {
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+ 
+   
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    
+    public static void main(Reverse String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = { 7,07,0,70,7,77};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
